30180124be8d2409e7aa3f77b0065190e0e27bc5
Alex <asurg17@freeuni.edu.ge>
2021-06-04 21:34:28 +0400

    visual part done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { ShellHeaderComponent } from './shell/shell-header/shell-header.componen
 import { ShellSidebarComponent } from './shell/shell-sidebar/shell-sidebar.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { ClientHeaderComponent } from './shell/client-header/client-header.component';
+import { Bpm000Component } from './shell/modules/bpm/bpm000/bpm000.component';
+import { Bpm001Component } from './shell/modules/bpm/bpm001/bpm001.component';
+import { KrnicpComponent } from './shell/modules/krn/krnicp/krnicp.component';
+import { OperationsComponent } from './shell/modules/krn/operations/operations.component';
+import { Pmd311Component } from './shell/modules/pmd/pmd311/pmd311.component';
+import { AccountsComponent } from './shell/modules/krn/accounts/accounts.component';
+import { CreateComponent } from './shell/modules/krn/accounts/create/create.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +27,15 @@ import { RegisterComponent } from './auth/register/register.component';
     ShellHeaderComponent,
     ShellSidebarComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ClientHeaderComponent,
+    Bpm000Component,
+    Bpm001Component,
+    KrnicpComponent,
+    OperationsComponent,
+    Pmd311Component,
+    AccountsComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
